3ee17355d827378a1830db5515b347d0f09074f3
techko <sasko.andonov@gmail.com>
2025-02-16 09:00:16 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,13 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Pure Fleet | Window Tinting & PPF",
   description:
-    "Pure Fleet provides top-notch mobile car cleaning services at your convenience. Book now for a pristine vehicle.",
+    "Pure Fleet provides top-notch Window Tinting, PPF & car detailing services at your convenience. Book now for a pristine vehicle.",
   keywords: "mobile car cleaning, Pure Fleet, car wash, professional cleaning",
   authors: [{name: "Pure Fleet"}],
   openGraph: {
     title: "Pure Fleet | Window Tinting & PPFg",
     description:
-      "Pure Fleet provides top-notch Window Tinting,PPF & car detailing services at your convenience. Book now for a pristine vehicle.",
+      "Pure Fleet provides top-notch Window Tinting, PPF & car detailing services at your convenience. Book now for a pristine vehicle.",
     images: [
       {
         url: "https://purefleet.me/gallery3.png",
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: "Pure Fleet | Window Tinting & PPF",
     description:
-      "Pure Fleet provides top-notch Window Tinting,PPF & car detailing  services at your convenience. Book now for a pristine vehicle.",
+      "Pure Fleet provides top-notch Window Tinting, PPF & car detailing  services at your convenience. Book now for a pristine vehicle.",
     images: ["https://purefleet.me/gallery3.png"],
   },
 };
